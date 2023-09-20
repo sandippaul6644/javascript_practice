d84e55b795c820485471ec64e7de362a78831c34
sandippaul6644 <79242053+sandippaul6644@users.noreply.github.com>
2023-09-20 07:46:43 +0000

    basic

--- basic/datatypes.js
+++ basic/datatypes.js
@@ -1,9 +1,26 @@
-let myNumber = 7318886644
-let myName = "sandip paul "
+var myNumber = 7318886644
+var myName = "sandip paul"
+let isLogedin = true
+let myPurchesedweight = 898.8988
+
+const number = null;
 
 
 
 
-console.log(myName + 'is a good boy') // sandippaul is a good boy
+
+
+
+
+
+console.log(myName + 'is a good boy') 
 console.log(myName) 
 console.log( typeof myName )
+
+console.log( typeof isLogedin )
+
+console.log( typeof myPurchesedweight )
+
+
+
+console.table([myName +" "+ 'is a good boy',typeof myName,typeof isLogedin,typeof myPurchesedweight, number ])
