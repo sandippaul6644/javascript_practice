0c609ac034a7de640103851c077f1bcae13ff48e
sandippaul6644 <79242053+sandippaul6644@users.noreply.github.com>
2023-11-28 07:40:53 +0000

    sc

---
+++ basic/conversionOperator.js
@@ -0,0 +1,28 @@
+let Score1 = 33
+let Score2 = "33"
+let Score3 = "33abc"
+
+let score5 = null
+let score6 = undefined
+let score7 = true 
+
+
+
+//conversion function
+
+let valueNumber = Number(Score3);
+
+console.log(valueNumber);
+
+console.log(typeof valueNumber);
+
+
+
+
+
+
+//console.log(typeof Score1);
+//console.log(typeof (Score2));
+//console.log(typeof Score3);
+
+//console.table([score5, score6, score7])
